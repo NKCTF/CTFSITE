a5fcf17ed1b99fc6682ef2a8e9d34e8406044174
Chinese-BruceWayne <1184249450@qq.com>
2018-10-13 13:11:27 +0800

    在生产模式下 关闭Debug模式 && 添加配置文件

--- ctfsite/prod.py
+++ ctfsite/prod.py
@@ -4,8 +4,27 @@ from .settings import *
 # 生产环境关闭DEBUG模式
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/www/NKCTF/ctfsite/ctfsite_debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 # 生产环境开启跨域
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 # 特别说明，下面这个不需要，因为前端是VueJS构建的，它默认使用static作为静态文件入口，我们nginx配置static为入口即可，保持一致，没Django什么事
 STATIC_URL = '/static/'
